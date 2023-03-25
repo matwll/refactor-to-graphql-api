62d232e836b3d07754599c51bb8e8cafefa95744
Matthew Williams <maw01091989@gmail.com>
2023-03-25 11:12:19 -0700

    making sure any changes are pushed

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -69,7 +69,7 @@ const SearchBooks = () => {
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
-
+    console.log(bookToSave);
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
@@ -79,10 +79,8 @@ const SearchBooks = () => {
 
     try {
       const { response } = await saveBook({
-        variables: { bookInfo: { ...bookToSave } },
+        variables: { bookData: { ...bookToSave } },
       });
-
-      console.log(response);
 
       if (!response.ok) {
         throw new Error("something went wrong!");
