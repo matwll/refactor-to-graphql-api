f328357f92ad9f62525c7a6cfeba05b2fa58c5a8
Matthew Williams <maw01091989@gmail.com>
2023-03-26 06:36:06 -0700

    ensuing recent changes are pushed

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -59,7 +59,7 @@ const resolvers = {
       if (context.user) {
         const updatedUser = await User.findByIdAndUpdate(
           { _id: context.user._id },
-          { $push: { savedBooks: { bookId: bookId } } },
+          { $pull: { savedBooks: { bookId: bookId } } },
           { new: true }
         );
         return updatedUser;
